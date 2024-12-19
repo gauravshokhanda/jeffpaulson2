a3b10af65cf4584ebcd873755b84811f65060ed3
Gagan <gd885973@gmail.com>
2024-12-19 16:59:13 +0530

    new project

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text className="text-3xl bg-red-600">Hello tailwind</Text>
+    </View>
+  );
+}
